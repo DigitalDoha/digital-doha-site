9b11617d7a1527634f380f7e1fb1e5cd2e302233
DigitalDoha <wolcostofficial@gmail.com>
2026-08-13 19:44:13 +0300

    Activate reference design while retaining 2026 profile content

--- content-loader.js
+++ content-loader.js
@@ -1,8 +1,1 @@
-(() => {
-  const src = document.currentScript?.src;
-  if (!src) return;
-  const loader = document.createElement('script');
-  loader.src = new URL('content-loader-2026.js', src).href;
-  loader.async = true;
-  document.head.appendChild(loader);
-})();
+(()=>{const ui=document.createElement('script');ui.src='/script-ui-v2.js';ui.onload=()=>{const profile=document.createElement('script');profile.src='/script-profile-v2.js';document.head.appendChild(profile);};document.head.appendChild(ui);})();
